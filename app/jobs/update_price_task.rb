dce4b92958abc2c867dce178f6c55ddb06f628b8
Lorenzo Benvenuti <lorenzo.benvenuti@gmail.com>
2019-03-06 17:13:02 +0100

    Handling exceptions in background job

--- app/jobs/update_price_task.rb
+++ app/jobs/update_price_task.rb
@@ -1,11 +1,16 @@
 class UpdatePriceTask
   def perform
     Item.all.each do |item|
-      Crono.logger.info("Retrieving price for #{item.name}")
-      service = ScraperService.for_host(URI(item.url).host)
-      price = service.get_price(item.url)
-      Crono.logger.info("Price = #{price}")
-      PriceHistoryService.new.update_price(item, price)
+      begin
+        Crono.logger.info("Retrieving price for #{item.name}")
+        service = ScraperService.for_host(URI(item.url).host)
+        price = service.get_price(item.url)
+        Crono.logger.info("Price = #{price}")
+        PriceHistoryService.new.update_price(item, price)
+      rescue StandardError => e
+        Crono.logger.error("Error #{e.message} occurred while updating price of #{item.name}")
+        Crono.logger.error("Backtrace: #{e.backtrace}")
+      end
     end
   end
 end
